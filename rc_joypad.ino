cba45509eff803b10618ca54e5e36a5ea77a2bf8
Fabio Chicout <fchicout@gmail.com>
2020-12-26 14:21:06 -0300

    some starting ideas, some fixes should be made for this code to work

--- rc_joypad.ino
+++ rc_joypad.ino
@@ -1,64 +1,142 @@
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-
 #include <SPI.h>
 #include "RF24.h"
- 
-// Cria o objeto do display para i2c 
-Adafruit_SSD1306 dsp(-1); 
+#include "printf.h"
 
-// Instância do Radio Transmissor
-RF24 radio(9,10) 
-// Endereço do Transmissor
-const byte address[6] = "00006"; 
  
-void setup()
+RF24 radio(7,8) 
+const byte addresses[][6] = {"RCJoyP", "2ndNod"}; 
+bool radioNumber = 0;
+bool role = 0; // Sender || Receiver
+
+void configureRadio()
 {
-	// Inicia o display com endereco padrão
-	dsp.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-	Serial.begin(9600);
 	radio.begin();
-	radio.openWritingPipe(address);
 	radio.setPALevel(RF24_PA_HIGH);
-	radio.stopListening();
+	// Open a writing and reading pipe on each radio, with opposite addresses
+	if(radioNumber){
+		radio.openWritingPipe(addresses[1]);
+		radio.openReadingPipe(1,addresses[0]);
+	}else{
+		radio.openWritingPipe(addresses[0]);
+		radio.openReadingPipe(1,addresses[1]);
+	}
+	radio.startListening();
+  	radio.printDetails();
 }
- 
+
+void setup(){
+	Serial.begin(115200);
+	Serial.println(F("--====Communications Log====--"));
+	
+	printf_begin();
+
+	configureRadio();  
+}
+
+uint32_t configTimer =  millis();
+
 void loop()
 {
-	// Inicia 
-	dsp.clearDisplay();//limpa a tela
-  
-	dsp.setTextColor(WHITE);//define o texto para branco (no display ficara azul)
- 
-	dsp.setTextSize(1);//define o tamanho do texto
-	dsp.println("POT1:");//escreve na tela a mensagem
- 
-	dsp.setTextSize(3);
-	dsp.println("2018");
-	dsp.display();//mostra as alteracoes no display, sem isso nao ira mostrar nada!!
-	delay(10);
-	
-	// for (int8_t i = 0; i < 64; i++)
-	// {
-	// 	dsp.drawLine(0, 0, 128, i, WHITE);//desenha uma linha
-	// 	dsp.display();//mostra na tela
-	// 	delay(1);
-	// }
- 
-	// for (int8_t i = 63; i > -1; i--)
-	// {
-	// 	dsp.drawLine(0, 0, 128, i, BLACK);
-	// 	dsp.display();
-	// 	delay(1);
-	// }
+
 }
 
-char[32] sendData(char* text){
-	radio.write(&text, sizeof(text));
-	radio.startListening();
-	if (radio.available()){
-		char text[32] = "";
-		radio.read(&text, sizeof(text));
+void receiveData(){
+	if ( role == 0 )
+	{
+		unsigned long got_time;
+
+		if( radio.available()){
+			
+			uint32_t failTimer = millis();                              // Variable for the received timestamp
+			while (radio.available()) {                                 // While there is data ready
+			if(millis()-failTimer > 500){
+				Serial.println("Radio available failure detected");
+				radio.failureDetected = true;
+				break;
+			}
+			radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
+			}
+			
+			radio.stopListening();                                      // First, stop listening so we can talk   
+			radio.write( &got_time, sizeof(unsigned long) );            // Send the final one back.      
+			radio.startListening();                                     // Now, resume listening so we catch the next packets.     
+			Serial.print(F("Sent response "));
+			Serial.println(got_time);  
+		}
 	}
-	delay(10);
+}
+
+void transmitData(){
+	if (role == 1)  {
+    
+    radio.stopListening();                                    // First, stop listening so we can talk.
+    
+    
+    Serial.println(F("Now sending"));
+
+    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
+     if (!radio.write( &start_time, sizeof(unsigned long) )){
+       Serial.println(F("failed"));
+     }
+        
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
+    }else{
+        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
+        
+        //Failure Handling:
+        uint32_t failTimer = millis();
+        while(radio.available()){                               //If available always returns true, there is a problem
+          if(millis() - failTimer > 250){
+            radio.failureDetected = true;
+            Serial.println("Radio available failure detected");
+            break;
+          }
+          radio.read( &got_time, sizeof(unsigned long) );
+        }
+        unsigned long end_time = micros();
+        
+        // Spew it
+        Serial.print(F("Sent "));
+        Serial.print(start_time);
+        Serial.print(F(", Got response "));
+        Serial.print(got_time);
+        Serial.print(F(", Round-trip delay "));
+        Serial.print(end_time-start_time);
+        Serial.println(F(" microseconds"));
+    }
+
+    // Try again 1s later
+    delay(1000);
+  }
+}
+
+void changeRoles(){
+	if ( Serial.available() ) 
+	{
+		char c = toupper(Serial.read());
+		if ( c == 'T' && role == 0 ){      
+		Serial.println(F("*** CHANGING TO TRANSMIT ROLE ***"));
+		role = 1;                  // Become the primary transmitter (ping out)
+		
+	}else if ( c == 'R' && role == 1 ){
+		Serial.println(F("*** CHANGING TO RECEIVE ROLE ***"));      
+		role = 0;                // Become the primary receiver (pong back)
+		radio.startListening();
+		
+		}
+	}
+
 }
